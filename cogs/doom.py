49497cde8f48b47bb481b2dc05ea5923f146b159
charlie <charlieschuy@gmail.com>
2019-02-11 17:18:38 -0800

    Add downloading from Dropbox and Google Drive

--- cogs/doom.py
+++ cogs/doom.py
@@ -6,6 +6,7 @@ import json
 import logging
 import re
 import subprocess
+from urllib.parse import parse_qs, urlparse, urlunparse
 import zipfile
 
 log = logging.getLogger('dev')
@@ -36,33 +37,70 @@ class DoomCog:
             return
 
         url = ''
+        urlIsZip = False
         gameName = ''
         if len(args) == 1:
             attachment = ctx.message.attachments[0]
             url = attachment.url
             gameName = attachment.filename.split('.')[0]
         else:
-            idgames = args[1]
-            match = re.search('doomworld\\.com\\/idgames', idgames)
-            if not match:
-                await ctx.send('Invalid idgames url')
-                await self.host_help(ctx)
-                return
-            url = self.config['idgamesMirror'] + idgames[match.end():] + '.zip'
-            gameName = idgames.split('/')[-1]
+            fileURL = urlparse(args[1])
+            if 'doomworld' in fileURL.netloc:
+                log.info('Obtaining wad from idgames mirror...')
+                idgames = args[1]
+                match = re.search('doomworld\\.com\\/idgames', idgames)
+                if not match:
+                    await ctx.send('Invalid idgames url')
+                    await self.host_help(ctx)
+                    return
+                url = self.config['idgamesMirror'] + idgames[match.end():] + '.zip'
+                gameName = idgames.split('/')[-1]
+                urlIsZip = True
+            elif 'dropbox.com' in fileURL.netloc:
+                log.info('Obtaining wad from Dropbox...')
+                url = urlunparse((fileURL.scheme, fileURL.netloc, fileURL.path, fileURL.params, 'raw=1', fileURL.fragment))
+            elif 'drive.google.com' in fileURL.netloc:
+                log.info('Obtaining wad from Google Drive...')
+                googleDriveId = ''
+                if fileURL.path == '/open':
+                    qs = parse_qs(fileURL.query)
+                    googleDriveId = qs['id'][0]
+                elif 'file/d' in fileURL.path:
+                    googleDriveId = fileURL.path.split('/')[3]
+                else:
+                    await ctx.send('Invalid Google Drive url')
+                    await self.host_help(ctx)
+                    return
+                url = 'https://drive.google.com/uc?export=download&id={0}'.format(googleDriveId)
 
         wads = []
         log.info('Attempting to download wad from {}'.format(url))
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status == 200:
-                    # This is synchronous which could potentially cause problems, but since
-                    # the bot only serves one server, I'm not going to worry about it now.
-                    with zipfile.ZipFile(io.BytesIO(await response.read()), 'r') as wadZip:
-                        for name in wadZip.namelist():
-                            if re.fullmatch('.*\\.wad', name):
-                                wadZip.extract(name, self.config['wadsDirectory'])
-                                wads.append(self.config['wadsDirectory'] + '/' + name)
+                    filename = ''
+                    # Check for Content-Disposition in the response header for obtaining the filename from
+                    # Dropbox and Google Drive downloads
+                    if 'Content-Disposition' in response.headers:
+                        match = re.search('filename="(.*)"', response.headers['Content-Disposition'])
+                        filename = match.group(1)
+                        gameName = filename[:-4]
+                        if filename[-4:].lower() == '.zip':
+                            urlIsZip = True
+
+                    if urlIsZip:
+                        # This is synchronous which could potentially cause problems, but since
+                        # the bot only serves one server, I'm not going to worry about it now.
+                        with zipfile.ZipFile(io.BytesIO(await response.read()), 'r') as wadZip:
+                            for name in wadZip.namelist():
+                                if re.fullmatch('.*\\.wad', name):
+                                    wadZip.extract(name, self.config['wadsDirectory'])
+                                    wads.append(self.config['wadsDirectory'] + '/' + name)
+                    elif filename[-4:].lower() == '.wad':
+                        path = self.config['wadsDirectory'] + '/' + filename
+                        wads.append(path)
+                        with open(path, mode='wb') as f:
+                            f.write(await response.read())
                 else:
                     log.warning('Could not download wad from {0}, response status: {1}'.format(url, response.status))
                     ctx.send('Could not download the wad')
